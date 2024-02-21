df9704d358222c71ab106bc74d50bc3da232b8ac
Keerthi Kumar S G <keerthikumarsg@Keerthis-MacBook-Air.local>
2024-02-21 17:41:17 +0530

    controller advice

--- src/main/java/com/example/product/controllers/ProductController.java
+++ src/main/java/com/example/product/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.product.controllers;
 
+import com.example.product.dtos.ErrorResponseDto;
 import com.example.product.dtos.ProductRequestDto;
 import com.example.product.dtos.ProductWrapper;
 import com.example.product.models.Product;
@@ -22,56 +23,37 @@ public class ProductController {
 
     // Get all the products
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
     // Get a product with Id
     @GetMapping("/products/{id}")
-    public ResponseEntity<ProductWrapper> getSingleProduct(@PathVariable("id") Long id)  {
+    public ResponseEntity<ProductWrapper> getSingleProduct(@PathVariable("id") Long id) throws InvalidProductIdException {
 
         ResponseEntity<ProductWrapper> response;
-        try {
-            Product singleProduct = productService.getSingleProduct(id);
-            ProductWrapper productWrapper = new ProductWrapper(singleProduct, "Successfully retried the data");
-            response = new ResponseEntity<>(productWrapper, HttpStatus.OK);
-        } catch (InvalidProductIdException e) {
-            ProductWrapper productWrapper = new ProductWrapper(null, "Product is not present ");
-            response =  new ResponseEntity<>(productWrapper, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        Product singleProduct = productService.getSingleProduct(id);
+        ProductWrapper productWrapper = new ProductWrapper(singleProduct, "Successfully retried the data");
+        response = new ResponseEntity<>(productWrapper, HttpStatus.OK);
         return response;
     }
 
     @PostMapping("/products")
-    public Product addProduct(@RequestBody ProductRequestDto productRequestDto){
+    public Product addProduct(@RequestBody ProductRequestDto productRequestDto) {
         return new Product();
     }
 
     @PutMapping("/products/{id}")
     public Product updateProduct(@PathVariable("id") Long id,
-                                 @RequestBody ProductRequestDto productRequestDto){
+                                 @RequestBody ProductRequestDto productRequestDto) {
         return productService.updateProduct(id, productRequestDto);
     }
 
+
     @DeleteMapping("/products/{id}")
-    public boolean deleteProduct(@PathVariable("id") Long id){
+    public boolean deleteProduct(@PathVariable("id") Long id) {
         return true;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
