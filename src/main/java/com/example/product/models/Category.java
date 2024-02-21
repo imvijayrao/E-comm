5e487cddd41fc19a579e516fe150d71ae561402f
Keerthi Kumar S G <keerthikumarsg@Keerthis-MacBook-Air.local>
2024-02-21 22:50:01 +0530

    inheritance in db using orm

--- src/main/java/com/example/product/models/Category.java
+++ src/main/java/com/example/product/models/Category.java
@@ -1,12 +1,13 @@
 package com.example.product.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Category {
+@Entity
+public class Category extends BaseModel{
 
-    private Long id;
     private String name;
 }
